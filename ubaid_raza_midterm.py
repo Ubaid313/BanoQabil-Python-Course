b20d332b5347d4d932cd8712c3f72eb0e03ee403
Ubaid313 <156326262+Ubaid313@users.noreply.github.com>
2024-01-18 21:59:07 +0500

    Add files via upload

---
+++ ubaid_raza_midterm.py
@@ -0,0 +1,34 @@
+#Ubaid Raza
+#Mid Term Exam 
+#Q1 - Write a Python program to do arithmetical operations addition 
+
+#First For Addition
+a=25
+b=50
+#Printing The Sum 
+sum=a+b
+print(sum)
+
+#Second For Division
+c=int(input("numerator"))
+d=int(input("demoninator"))
+answer=c/d
+print("The division is:",answer) 
+
+#Q2 - Write a Python program to find the area of a triangle
+base= int(input("base of triangle"))
+height= int(input("height of triangle"))
+area=base*height*0.5
+print("the area of triangle is:", area)
+
+#Q3 - Write a Python program to convert Celsius to Fahrenheit.
+calsius=20
+fahrenheit= calsius*9/5 +32
+print("conversion:",fahrenheit)
+
+
+#Q4 - Write a Python program that return all datatypes that we discussed in the class
+int=5
+str="ubaid"
+float=0.5
+bool= False or True 
